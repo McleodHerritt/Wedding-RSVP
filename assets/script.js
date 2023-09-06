a0d40b83c7735d199e41edab89391b8f2437a784
McleodHerritt <nmcleod2021@gmail.com>
2023-09-06 19:42:18 -0300

    added plus one option to html form and added function in js to collect data

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ form.addEventListener("submit", submitForm);
 var data = {
   guestName: "",
   attendance: false,
+  plusOne: false,
+  guestOne: "",
   // add additional keys to object to send to email
 };
 
@@ -15,14 +17,24 @@ function submitForm(event) {
     'input[name="attendance"]:checked'
   ).value;
 
-  //TODO: get additional information before sending to email
+  data.plusOne = document.querySelector('input[name="plusOne"]:checked').value;
+
+  data.guestOne = document.getElementById("guestOne").value;
+
+  //   TODO: get additional information before sending to email
   sendDataToEmail();
 
   document.getElementById("name").value = "";
   document.querySelector('input[name="attendance"]:checked').checked = false;
+  document.getElementById("guestOne").value = "";
+  data.guestOne = document.querySelector(
+    'input[name="plusOne"]:checked'
+  ).checked = false;
 }
 
 function sendDataToEmail() {
   console.log(data.guestName);
   console.log(data.attendance);
+  console.log(data.plusOne);
+  console.log(data.guestOne);
 }
