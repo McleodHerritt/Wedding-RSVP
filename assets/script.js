510198d491bdc44fc151e95f35de6337d92d789a
Riley <rileyg0035@gmail.com>
2023-09-07 19:18:38 -0300

    added timer

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,31 @@ function sendDataToEmail() {
   console.log(data.plusOne);
   console.log(data.guestOne);
 }
+
+
+//Countdown 
+
+const targetDate = new Date("2024-05-03T23:00:00").getTime();
+
+ 
+const countdownInterval = setInterval(function() {
+    const currentDate = new Date().getTime();
+    const timeRemaining = targetDate - currentDate;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    
+    const timerElement = document.getElementById("countdown-timer");
+    timerElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (timeRemaining <= 0) {
+        clearInterval(countdownInterval);
+        timerElement.innerHTML = "";
+    }
+}, 1000); 
+
+
+//
